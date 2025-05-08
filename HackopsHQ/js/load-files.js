03805280f97421f28ced19ed7f153cb4f1593b99
ShadowBye09 <knoxrichard316@gmail.com>
2025-05-08 07:23:50 -1000

    Update load-files.js

--- HackopsHQ/js/load-files.js
+++ HackopsHQ/js/load-files.js
@@ -1,15 +1,14 @@
-// load a simple JSON manifest at /files/manifest.json
 fetch('/files/manifest.json')
   .then(res => res.json())
   .then(json => {
-    const list = document.querySelector('#file-list ul');
+    const listEl = document.querySelector('#file-list ul');
     json.files.forEach(f => {
       const li = document.createElement('li');
       const a  = document.createElement('a');
       a.href = `/files/${f}`;
       a.textContent = f;
       li.appendChild(a);
-      list.appendChild(li);
+      listEl.appendChild(li);
     });
   })
   .catch(err => console.error('Could not load manifest.json', err));
